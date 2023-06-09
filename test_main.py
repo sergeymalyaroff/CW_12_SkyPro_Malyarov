ae86ea19bc683475e1720a277e3711451b888224
Sergey Malyarov <sergey@malyaroff.com>
2023-06-09 13:31:42 +0300

    Initial commit

---
+++ test_main.py
@@ -0,0 +1,8 @@
+import unittest
+
+class TestMain(unittest.TestCase):
+    def test_example(self):
+        self.assertEqual(2 + 2, 4)  # Пример теста
+
+if __name__ == '__main__':
+    unittest.main()
